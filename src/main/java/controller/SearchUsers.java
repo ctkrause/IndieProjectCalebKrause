825fff2e9c3f9db56b2a3044274d8f5f3712b3c2
ctkrause <ckrause2@madisoncollege.edu>
2021-02-27 17:58:16 -0600

    added servlet to display all users and use as a landing page for the time being

---
+++ src/main/java/controller/SearchUsers.java
@@ -0,0 +1,30 @@
+package controller;
+
+import persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/index"}
+)
+
+public class SearchUsers extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        GenericDao userDao = new GenericDao();
+        if (req.getParameter("submit").equals("search")) {
+            req.setAttribute("users", userDao.getAll(req.getParameter("searchTerm")));
+        } else {
+            req.setAttribute("users", userDao.getAll());
+        }
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/results.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
